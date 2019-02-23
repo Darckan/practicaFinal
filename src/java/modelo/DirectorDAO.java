c07b06e6b26fb58a5d9d33972f5db6e4a9292ab3
Darckan <remushurmuz4@gmail.com>
2019-02-23 14:45:06 +0100

    DirectorDAO modificar existeDirector, añadir consultarDirector, y insertarDirector.

--- src/java/modelo/DirectorDAO.java
+++ src/java/modelo/DirectorDAO.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  */
 public class DirectorDAO {
     
-    public static boolean exiteDirector( String director ){
+    public static boolean exiteDirector( String nombre, String apellidos ){
         Statement st;
         ResultSet res;
         boolean resultado = false;
@@ -31,7 +31,7 @@ public class DirectorDAO {
             // Ahora construimos la lista
             while (res.next()){
                 
-                if(res.getString("director").equals(director)){
+                if(res.getString("nombre").equals(nombre) && res.getString("apellidos").equals(apellidos)){
                     resultado = true;
                 }
             }
@@ -49,7 +49,79 @@ public class DirectorDAO {
         return resultado;  
     }
     
+    public static int insertarDirector(String nombre, String apellidos, int edad){
+        
+        // Cadena con la consulta parametrizada
+        String sql = "insert into directores values (?,?,?)";
+
+        Conexion conexion = new Conexion();
+        
+        PreparedStatement prest;
+
+        try { 
+            // Preparamos la inserción de datos  mediante un PreparedStatement
+            prest = conexion.getConexion().prepareStatement(sql);
+
+            // Procedemos a indicar los valores que queremos insertar
+            // Usamos los métodos setXXX(indice, valor)
+            // indice indica la posicion del argumento ?, empieza en 1
+            // valor es el dato que queremos insertar
+            prest.setString(1, nombre);
+            prest.setString(2, apellidos);
+            prest.setInt(3, edad);
+
+            // Ejecutamos la sentencia de inserción preparada anteriormente
+            int nfilas = prest.executeUpdate();
     
+            // Cerramos el recurso PreparedStatement 
+            prest.close();
+            
+            // Cerramos la conexión 
+            conexion.cerrarConexion();
+            // La inserción se realizó con éxito, devolvemos filas afectadas
+            return nfilas;
+        } catch (SQLException e) {
+            System.out.println("Problemas durante la inserción de datos en la tabla directores");
+            System.out.println(e);
+            return -1;
+        }
+    }
+    
+    public static Director consultarDirector(String nombre, String apellidos){
+        Statement st;
+        ResultSet res;
+        Director director = new Director();
+        
+        // Guardo la consulta SQL realizar en una cadena
+        String sql = "select * from directores where nombre ='"+nombre+"' and apellidos='"+apellidos+"'";
+  
+        Conexion conexion = new Conexion();
+        
+        try {
+            
+            // Preparamos Statement
+            st = conexion.getConexion().createStatement(); 
+            // Ejecutamos la sentencia y obtenemos la tabla resultado
+            res = st.executeQuery(sql);
+            // Ahora construimos la lista
+            director.setId_director(res.getInt("id"));
+            director.setNombre(res.getString("nombre"));
+            director.setApellidos(res.getString("apellidos"));
+            director.setEdad(res.getInt("edad"));
+            
+            // Cerramos el recurso PreparedStatement 
+            st.close();
+            // Cerramos la conexión 
+            conexion.cerrarConexion();
+            // La inserción se realizó con éxito, devolvemos filas afectadas
+        } catch (SQLException e) {
+            System.out.println("Problemas durante la consulta en tabla directores");
+            System.out.println(e);
+            
+        }
+
+        return director;  
+    }
    
     
 }
