61cb293e2478bc96623750ab715cbe9b2235a273
Darckan <remushurmuz4@gmail.com>
2019-02-23 13:59:05 +0100

    Pelicula, atributos, getter, setter y toString

---
+++ src/java/modelo/Pelicula.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author darck
+ */
+public class Pelicula {
+    
+    private int id_pelidula;
+    private String nombre;
+    private int id_director;
+    private int costes;
+}
