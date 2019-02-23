d8a9335034faf8b7e590b83061b3c92b788aa3a3
Darckan <remushurmuz4@gmail.com>
2019-02-23 13:57:09 +0100

    Director, atributos, getter, setter y toString

---
+++ src/java/modelo/Director.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author darck
+ */
+public class Director {
+    
+    private int id_director;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public int getId_director() {
+        return id_director;
+    }
+
+    public void setId_director(int id_director) {
+        this.id_director = id_director;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Director{" + "id_director=" + id_director + ", nombre=" + nombre + ", apellidos=" + apellidos + ", edad=" + edad + '}';
+    }
+    
+}
