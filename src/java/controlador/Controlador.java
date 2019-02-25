af649cf1bca3868a6bcb87147363bd5917fcebaf
Darckan <remushurmuz4@gmail.com>
2019-02-25 20:11:04 +0100

    Controlador añadida configuracion para borrar pelicula, borrar director, insertar pelicula, insertar director y ver peliculas de un director

--- src/java/controlador/Controlador.java
+++ src/java/controlador/Controlador.java
@@ -48,16 +48,16 @@ public class Controlador extends HttpServlet{
 
             int filas = PeliculaDAO.actualizarPelicula(editPeliculas, directorEditPeli, nombreEditPeli, costesEditPeli );
             
-            response.sendRedirect(response.encodeRedirectURL("index.jsp?" + filas));
-
             // Una vez realizada la operación, redirigimos a la vista
+            response.sendRedirect(response.encodeRedirectURL("index.jsp?" + filas));
+            
         } else if(accion.equals("editarDirector1")){
             
             String editDirectores = request.getParameter("editDirectores");
             sesion.setAttribute("editDirectores", editDirectores);
             
             response.sendRedirect(response.encodeRedirectURL("editDirector.jsp"));
-        }else if(accion.equals("editarDirector2")){
+        } else if(accion.equals("editarDirector2")){
             
             int editDirectores = Integer.parseInt(request.getParameter("editDirectores"));
             String nombreEditDirector = request.getParameter("nombreEditDirector");
@@ -66,22 +66,79 @@ public class Controlador extends HttpServlet{
 
             int filas = DirectorDAO.actualizarDirector(editDirectores, nombreEditDirector, apellidosEditDirector, edadEditDirector );
             
+            // Una vez realizada la operación, redirigimos a la vista
             response.sendRedirect(response.encodeRedirectURL("index.jsp?" + filas));
+            
+        } else if(accion.equals("borrarPelicula")){
+                     
+            int id_pelicula = Integer.parseInt(request.getParameter("borraPeliculas"));
+            int filas = PeliculaDAO.borrarPelicula(id_pelicula);
 
             // Una vez realizada la operación, redirigimos a la vista
+            response.sendRedirect(response.encodeRedirectURL("index.jsp?" + filas));
+
+        } else if(accion.equals("borrarDirector")){
+            
+            int id_director = Integer.parseInt(request.getParameter("borraDirector"));
+            int filas = DirectorDAO.borrarDirector(id_director);
+
+            // Una vez realizada la operación, redirigimos a la vista
+            response.sendRedirect(response.encodeRedirectURL("index.jsp?" + filas));
+
+        } else if(accion.equals("verPeliculasDirector")){
+            
+            String directorId = request.getParameter("verPelis");
+        
+            // Asigno ese id del director al atributo de la sesión y así lo puedo usar en la vista
+            sesion.setAttribute("directorId", directorId);
+
+            // Una vez realizada la operación, redirigimos a la vista
+            response.sendRedirect(response.encodeRedirectURL("PeliculasDeDirector.jsp"));
+
+        } else if(accion.equals("insertDirector")){
+            
+            String nombreDirect = request.getParameter("nombreDirect");
+            String apeDirect = request.getParameter("apeDirect");
+            int edadDirect = Integer.parseInt(request.getParameter("edadDirect"));
+            int filas = 0;
+
+            if (DirectorDAO.exiteDirector(nombreDirect, apeDirect)){
+                response.sendRedirect(response.encodeRedirectURL("ErrorDire.jsp"));
+            }else{
+                filas = DirectorDAO.insertarDirector(nombreDirect, apeDirect, edadDirect);
+            }
+
+            // Una vez realizada la operación, redirigimos a la vista
+            response.sendRedirect(response.encodeRedirectURL("index.jsp?" + filas));
+            
+        } else if(accion.equals("insertPelicula")){
+            
+            String nombrePeli = request.getParameter("nombrePeli");
+            int costesPeli = Integer.parseInt(request.getParameter("costesPeli"));
+            int directorPeli = Integer.parseInt(request.getParameter("directorPeli"));
+            int filas = 0;
+
+            if (PeliculaDAO.exitePelicula(nombrePeli)){
+                response.sendRedirect(response.encodeRedirectURL("ErrorPeli.jsp"));
+            }else{
+                filas = PeliculaDAO.insertarPelicula(nombrePeli, directorPeli, costesPeli);
+            }
+
+            // Una vez realizada la operación, redirigimos a la vista
+            response.sendRedirect(response.encodeRedirectURL("index.jsp?" + filas));
+            
+        } else{
+            response.sendRedirect(response.encodeRedirectURL("index.jsp?algofallo"));
         }
 
         
         
     }
-    
-
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
